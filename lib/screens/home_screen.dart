d7a5e3cfc0c129f9ed4e5505dcf7fbf2f41c956e
mensur056 <mansur.sarxanov@gmail.com>
2021-12-30 10:24:15 +0400

    My MusicApp

--- lib/screens/home_screen.dart
+++ lib/screens/home_screen.dart
@@ -14,17 +14,34 @@ class _HomeScreenState extends State<HomeScreen> {
       appBar: AppBar(
         backgroundColor: black,
         elevation: 0,
-        title: Row(
-          mainAxisAlignment: MainAxisAlignment.spaceBetween,
-          children: [
-            Text(
-              'Explore',
-              style:
-                  TextStyle(color: Colors.white, fontWeight: FontWeight.bold),
-            ),
-            Icon(Icons.list),
-          ],
+        title: Padding(
+          padding: const EdgeInsets.only(left: 10.0, right: 10),
+          child: Row(
+            mainAxisAlignment: MainAxisAlignment.spaceBetween,
+            children: [
+              Text(
+                'Explore',
+                style:
+                    TextStyle(color: Colors.white, fontWeight: FontWeight.bold),
+              ),
+              Icon(Icons.list),
+            ],
+          ),
         ),
+      ),
+      body: getBody(),
+    );
+  }
+
+  Widget getBody() {
+    return SingleChildScrollView(
+      child: Column(
+        crossAxisAlignment: CrossAxisAlignment.start,
+        children: [
+          Column( crossAxisAlignment: CrossAxisAlignment.start,
+            children: [],
+          )
+        ],
       ),
     );
   }
