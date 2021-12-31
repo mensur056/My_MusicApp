983fd5c421852ebb8e759b2ab66f7a20bc9e7e65
mensur056 <mansur.sarxanov@gmail.com>
2021-12-31 10:39:31 +0400

    My MusicApp

---
+++ lib/screens/album_screen.dart
@@ -0,0 +1,17 @@
+import 'package:flutter/material.dart';
+
+class AlbumPage extends StatefulWidget {
+  final dynamic song;
+
+  const AlbumPage({this.song});
+
+  @override
+  _AlbumPageState createState() => _AlbumPageState();
+}
+
+class _AlbumPageState extends State<AlbumPage> {
+  @override
+  Widget build(BuildContext context) {
+    return Container();
+  }
+}
