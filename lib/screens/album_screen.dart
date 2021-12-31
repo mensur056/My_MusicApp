d0b14952c79b7c788ece3e8619b82b9e8f8c62f0
mensur056 <mansur.sarxanov@gmail.com>
2021-12-31 13:31:14 +0400

    My MusicApp

--- lib/screens/album_screen.dart
+++ lib/screens/album_screen.dart
@@ -1,5 +1,7 @@
 import 'package:flutter/material.dart';
 import 'package:my_musicapp/constants.dart';
+import 'package:my_musicapp/json/songs_json.dart';
+import 'package:page_transition/page_transition.dart';
 
 class AlbumPage extends StatefulWidget {
   final dynamic song;
@@ -37,23 +39,100 @@ class _AlbumPageState extends State<AlbumPage> {
               SizedBox(
                 height: 30,
               ),
-              Row(
-                mainAxisAlignment: MainAxisAlignment.spaceBetween,
-                children: [
-                  Text(
-                    widget.song['title'],
-                    style: TextStyle(
-                        color: Colors.white, fontWeight: FontWeight.bold),
-                  ),
-                  Container(
-                    decoration: BoxDecoration(
-                      borderRadius: BorderRadius.circular(5),
-                      color: Colors.grey,
+              Padding(
+                padding: const EdgeInsets.only(left: 30.0, right: 30),
+                child: Row(
+                  mainAxisAlignment: MainAxisAlignment.spaceBetween,
+                  children: [
+                    Text(
+                      widget.song['title'],
+                      style: TextStyle(
+                          fontSize: 18,
+                          color: Colors.white,
+                          fontWeight: FontWeight.bold),
                     ),
-                    child: Text('Subscribe'),
-                  )
-                ],
-              )
+                    Container(
+                      decoration: BoxDecoration(
+                        borderRadius: BorderRadius.circular(5),
+                        color: Colors.grey,
+                      ),
+                      child: Padding(
+                        padding: const EdgeInsets.only(
+                            left: 12.0, right: 12, top: 8, bottom: 8),
+                        child: Text('Subscribe'),
+                      ),
+                    )
+                  ],
+                ),
+              ),SizedBox(height: 30,),
+              SingleChildScrollView(
+                scrollDirection: Axis.horizontal,
+                child: Padding(
+                  padding: const EdgeInsets.only(left: 30.0),
+                  child: Row(
+                    children: List.generate(
+                      songs.length,
+                          (index) {
+                        return Padding(
+                          padding: EdgeInsets.only(right: 30),
+                          child: GestureDetector(
+                            onTap: () { Navigator.push(
+                                context,
+                                PageTransition(
+                                    alignment: Alignment.bottomCenter,
+                                    child: AlbumPage(
+                                      song: songs[index],
+                                    ),
+                                    type: PageTransitionType.scale));},
+                            child: Column(
+                              children: [
+                                Container(
+                                  height: 180,
+                                  width: 180,
+                                  decoration: BoxDecoration(
+                                    image: DecorationImage(
+                                        image: AssetImage(
+                                          songs[index]['img'],
+                                        ),
+                                        fit: BoxFit.cover),
+                                    color: Colors.green,
+                                    borderRadius: BorderRadius.circular(10),
+                                  ),
+                                ),
+                                SizedBox(
+                                  height: 18,
+                                ),
+                                Text(
+                                  songs[index]['title'],
+                                  style: TextStyle(
+                                      color: Colors.white,
+                                      fontSize: 15,
+                                      fontWeight: FontWeight.w600),
+                                ),
+                                SizedBox(
+                                  height: 10,
+                                ),
+                                Container(
+                                  child: Text(
+                                    songs[index]['description'],
+                                    maxLines: 1,
+                                    textAlign: TextAlign.center,
+                                    style: TextStyle(
+                                        color: Colors.grey,
+                                        fontSize: 12,
+                                        fontWeight: FontWeight.w600),
+                                  ),
+                                  width: 180,
+                                )
+                              ],
+                            ),
+                          ),
+                        );
+                      },
+                    ),
+                  ),
+                ),
+              ),
             ],
           ),
           SafeArea(
