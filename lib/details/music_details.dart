c996d99b2df786585aecc8b843aa36979c8f7591
mensur056 <mansur.sarxanov@gmail.com>
2021-12-31 17:47:37 +0400

    My MusicApp

---
+++ lib/details/music_details.dart
@@ -0,0 +1,15 @@
+import 'package:flutter/material.dart';
+
+class MusicDetailPage extends StatefulWidget {
+  const MusicDetailPage({Key? key}) : super(key: key);
+
+  @override
+  _MusicDetailPageState createState() => _MusicDetailPageState();
+}
+
+class _MusicDetailPageState extends State<MusicDetailPage> {
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold();
+  }
+}
